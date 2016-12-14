70d5b03ba3f4bbaba089c8568b40c2deb78ceac3
Anders Stalheim Øfsdahl <anders@apt.no>
2016-12-14 13:41:43 +0100

    Disable caching of generated modernizr code by default, configurable in .modernizr-autorc file

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
 var customizr = require('customizr');
 var extend = require('extend');
 
+var defaultConfig = {
+  // Based on default settings on https://github.com/Modernizr/customizr
+  options: ['setClasses', 'addTest', 'html5printshiv', 'testProp', 'fnBind'],
+  files: {
+    src: ['**[^node_modules]/**/*.{js,css,scss}'],
+  },
+  runOnce: false,
+};
+
 function wrapOutput(output) {
   // Expose generated Modernizr as a self executing module.
   return ";(function(window){\n" +
@@ -12,21 +21,13 @@ function wrapOutput(output) {
 }
 
 module.exports = function (config) {
-  if (typeof this.cacheable === 'function') {
+  var settings = extend(defaultConfig, JSON.parse(config));
+
+  if (typeof this.cacheable === 'function' && settings.runOnce) {
     this.cacheable()
   }
 
   var cb = this.async();
-
-  var def = {
-    // Based on default settings on https://github.com/Modernizr/customizr
-    options: ['setClasses', 'addTest', 'html5printshiv', 'testProp', 'fnBind'],
-    files: {
-      src: ['**[^node_modules]/**/*.{js,css,scss}'],
-    },
-  };
-
-  var settings = extend(def, JSON.parse(config));
 
   customizr(settings, function (data) {
     cb(null, wrapOutput(data.result));
